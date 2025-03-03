992070a6df99ad6c725713edfa2f6bfa6ac009f3
illirel <ilya.kononov.09@gmail.com>
2025-03-03 14:21:08 +0500

    RO-planer Plus

---
+++ src/home/Registration.tsx
@@ -0,0 +1,27 @@
+import styles from './LogIn.module.css';
+
+function Registration() {
+  return (
+    <div className={styles.logIn}>
+      <h1></h1>
+      <form>
+        <h2>
+          Cоздайте Вашу <br></br> учетную запись
+        </h2>
+        <input type='username' placeholder='Введите 4-3-1' />
+        <input type='password' placeholder='Введите пароль' />
+        <button>Войти</button>
+        <section>
+          <div>
+            <a href='#'>Регистрация</a>
+          </div>
+        </section>
+      </form>
+      <footer>
+        <img src='./OFSLogoHoriz.png' alt='Home' />
+      </footer>
+    </div>
+  );
+}
+
+export default Registration;
